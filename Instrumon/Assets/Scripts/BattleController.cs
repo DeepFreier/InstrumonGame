bfb146c3925f4da1b9b61dbebacc6f37a67cce32
Michael Freier <40905921+DeepFreier@users.noreply.github.com>
2024-04-03 20:43:03 -0400

    Added Functionality
    -created damage formula function
    -added taking in names and displaying them above the healthbars
    -changes the sprite on scene load
    -still need to link everything up, but the groundwork is there

--- Instrumon/Assets/Scripts/BattleController.cs
+++ Instrumon/Assets/Scripts/BattleController.cs
@@ -1,36 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 public class BattleController : MonoBehaviour
 {
+    public SpriteRenderer playerSpriteHolder;
+    public TextMeshProUGUI playerNameText;
     public Image playerHealthBar;
     private float playerCurrentHealth;
     private float playerTotalHealth;
     public TextMeshProUGUI playerCurrentHealthText;
     public TextMeshProUGUI playerTotalHealthText;
 
+    public SpriteRenderer oppSpriteHolder;
+    public TextMeshProUGUI oppNameText;
     public Image oppHealthBar;
     private float oppCurrentHealth;
     private float oppTotalHealth;
     public TextMeshProUGUI oppCurrentHealthText;
     public TextMeshProUGUI oppTotalHealthText;
 
+    public Sprite playerSprite;
+    private String playerName = "Trumpig";
     private int playerCurrentHP = 80;
     private int playerMonHP = 90;
+    private int playerMonAtk = 70;
     private int PlayerMonSpd = 5;
+
+    public Sprite oppSprite;
+    private String oppName = "Cymbalisk";
     private int oppMonHP = 120;
+    private int oppMonAtk = 50;
     private int OpponentMonSpd = 4;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        playerSpriteHolder.sprite = playerSprite;
+        playerNameText.text = playerName.ToString();
         playerCurrentHealth = playerCurrentHP;
         playerTotalHealth = playerMonHP;
         takeDamage(0);
         playerTotalHealthText.text = playerMonHP.ToString();
 
+        oppSpriteHolder.sprite = oppSprite;
+        oppNameText.text = oppName.ToString();
         oppCurrentHealth = oppMonHP;
         oppTotalHealth = oppMonHP;
         dealDamage(0);
@@ -43,7 +60,7 @@ public class BattleController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            takeDamage(5);
+            takeDamage(calcDamage(playerMonHP, 50, 30));
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -60,9 +77,18 @@ public class BattleController : MonoBehaviour
         }
     }
 
+    public int calcDamage(int totalHP, int atkStat, int atkPow)
+    {
+        return Math.Abs(atkStat + atkPow - totalHP/2);
+    }
+
     public void takeDamage(float damage)
     {
         playerCurrentHealth -= damage;
+        if (playerCurrentHealth < 0) 
+        {
+            playerCurrentHealth = 0;
+        }
         playerHealthBar.fillAmount = playerCurrentHealth / playerTotalHealth;
         playerCurrentHealthText.text = playerCurrentHealth.ToString();
     }
@@ -70,6 +96,10 @@ public class BattleController : MonoBehaviour
     public void dealDamage(float damage)
     {
         oppCurrentHealth -= damage;
+        if (oppCurrentHealth < 0)
+        {
+            oppCurrentHealth = 0;
+        }
         oppHealthBar.fillAmount = oppCurrentHealth / oppTotalHealth;
         oppCurrentHealthText.text = oppCurrentHealth.ToString();
     }
@@ -77,6 +107,10 @@ public class BattleController : MonoBehaviour
     public void healDamage(float heal)
     {
         playerCurrentHealth += heal;
+        if (playerCurrentHealth > playerTotalHealth)
+        {
+            playerCurrentHealth = playerTotalHealth;
+        }
         playerHealthBar.fillAmount = playerCurrentHealth / playerTotalHealth;
         playerCurrentHealthText.text = playerCurrentHealth.ToString();
     }
@@ -84,6 +118,10 @@ public class BattleController : MonoBehaviour
     public void healOppDamage(float heal)
     {
         oppCurrentHealth += heal;
+        if (oppCurrentHealth > oppTotalHealth)
+        {
+            oppCurrentHealth = oppTotalHealth;
+        }
         oppHealthBar.fillAmount = oppCurrentHealth / oppTotalHealth;
         oppCurrentHealthText.text = oppCurrentHealth.ToString();
     }
