86f3088cb404f1f1d4446e8ddd0431383d8ebfaf
BrandonM2019 <125689580+BrandonM2019@users.noreply.github.com>
2024-04-15 19:24:20 -0400

    Created a file to allow a list of party pokemon and added a way for moves to have PP added
    Along with variables for someone to use regarding the current HP for post-battle Instrumon

--- Instrumon/Assets/Scripts/Move.cs
+++ Instrumon/Assets/Scripts/Move.cs
@@ -6,4 +6,10 @@ public class Move
 {
     //This is here to placehold in case we ever want to implement PP as a changeable variable.
     public MoveBase Base { get; set; }
+    public int PP { get; set; }
+
+    public Move(MoveBase iBase){
+        Base = iBase;
+        PP = iBase.PP;
+    }
 }
