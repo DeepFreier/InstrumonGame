e326620ea9dda9f6275aed4500f433454d9d8f4e
patrickh48 <patrickhernandez4484@gmail.com>
2024-04-27 16:19:13 -0400

    Fixed the bug where the volume didn't update until options menu was opened

--- Instrumon/Assets/Scripts/World Layer Controllers/AudioController.cs
+++ Instrumon/Assets/Scripts/World Layer Controllers/AudioController.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class AudioController : MonoBehaviour
 {
     [SerializeField] AudioSource musicSource;
-
     public AudioClip background;
-
-
-
 
     private void Awake()
     {
+        LoadVolume();
         musicSource.clip = background;
         musicSource.Play();
+    }
+
+    private void LoadVolume()
+    {
+        if (PlayerPrefs.HasKey("Music"))
+        {
+            float volume = PlayerPrefs.GetFloat("Music");
+            AudioListener.volume = volume;
+        }
     }
 }
