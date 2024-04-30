25bab833c51fc9148c1c8f16f84e3f1490d58124
patrickh48 <patrickhernandez4484@gmail.com>
2024-04-29 23:31:22 -0400

    Fixed the progress screen text not showing up
    the progress screen in pause menu will now show a text blurb that details what is left to do in the game

---
+++ Instrumon/Assets/Scripts/ProgressTextUpdater.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
+using static UnityEngine.InputManagerEntry;
+
+public class ProgressTextUpdater : MonoBehaviour
+{
+    public TMP_Text progressText;
+
+    private void Update()
+    {
+        UpdateProgressText();
+    }
+
+    public void UpdateProgressText()
+    {
+        if (ProgressFlags.GetFlag() == 1)
+        {
+            progressText.text = $"The player has defeated {(ProgressFlags.GetFlag() - 1).ToString()} musicians. Find the first musician somewhere near the seadocks.";
+        }
+        else if (ProgressFlags.GetFlag() == 2)
+        {
+            progressText.text = $"The player has defeated {(ProgressFlags.GetFlag() - 1).ToString()} musician. Find the next musician somewhere in the world. Words echo in your mind:\n\n\"You should find the Musician Frank next, he's somewhere up north.\"";
+        }
+        else if (ProgressFlags.GetFlag() == 3)
+        {
+            progressText.text = $"The player has defeated {(ProgressFlags.GetFlag() - 1).ToString()} musicians. Find the next musician somewhere in the world. Words echo in your mind:\n\n\"You should find Karl next. He's in the park in the west side of town.\"";
+        }
+        else if (ProgressFlags.GetFlag() == 4)
+        {
+            progressText.text = $"The player has defeated {(ProgressFlags.GetFlag() - 1).ToString()} musicians. Find the next musician somewhere in the world. Words echo in your mind:\n\n\"Go ahead and find Joseph near the northern tunnel.\"";
+        }
+        else if (ProgressFlags.GetFlag() == 5)
+        {
+            progressText.text = $"The player has defeated all {(ProgressFlags.GetFlag() - 1).ToString()} musicians. Congradulations! You have beat the game!";
+        }
+    }
+}
